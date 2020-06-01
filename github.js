5a84a1fe870050c59cf962efcd74d6e227698066
Madalin Puscas <madalin@ramp-ops.com>
2020-06-01 13:25:25 +0300

    abc

---
+++ github.js
@@ -0,0 +1,23 @@
+class GitHub {
+    constructor() {
+        this.client_id = '37ea4195ab44ab71a9c3';
+        this.client_secret = '98249dda5fbd621a7ab8de2414c364825e6cb1f3';
+        this.repos_count = 5;
+        this.repos_sort = 'created: asc';
+    }
+
+    async getUser(user) {
+        const profileResponse = await fetch(`https://api.github.com/users/${user}?client_id=${this.client_id}&client_secret=${this.client_secret}`);
+
+        const repoResponse = await fetch(`https://api.github.com/users/${user}/repos?per_page=${this.repos_count}&sort=${this.repos_sort}&client_id=${this.client_id}&client_secret=${this.client_secret}`);
+
+        const profile = await profileResponse.json();
+
+        const repos = await repoResponse.json();
+
+        return {
+            profile,
+            repos
+        }
+    }
+}
